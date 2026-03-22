d714eecfd1d54b4fdc0b87e70da83cb7f14b77c4
Igor Moraes <igorfmoraes@proton.me>
2026-03-22 16:37:19 -0300

    GNOME Configurations
    Keyboard Shortcuts, Nightlight, Clock Format and Background image

--- configuration scripts/theming.sh
+++ configuration scripts/theming.sh
@@ -20,16 +20,20 @@ cd Graphite-cursors
 gsettings set org.gnome.desktop.interface cursor-theme "Graphite-dark-nord-cursors"
 cd ..
 
-# Firefox Theme
-curl -s -o- https://raw.githubusercontent.com/rafaelmardojai/firefox-gnome-theme/master/scripts/install-by-curl.sh | bash
-
 # Terminal
 gsettings set org.gnome.Ptyxis.Profile:/org/gnome/Ptyxis/Profiles/$PTYXIS_PROFILE/ palette nord
 gsettings set org.gnome.Ptyxis interface-style light
 
 # Background
-gsettings set org.gnome.desktop.background picture-uri resources/bg.jpg
-gsettings set org.gnome.desktop.background picture-uri-dark resources/bg.jpg
+cp -n resources/bg.jpg $HOME/.local/share/backgrounds/
+gsettings set org.gnome.desktop.background picture-uri file://$HOME/.local/share/backgrounds/bg.jpg
+gsettings set org.gnome.desktop.background picture-uri-dark file://$HOME/.local/share/backgrounds/bg.jpg
+
+# Stuff
+gsettings set org.gnome.desktop.interface clock-format '24h'
+gsettings set org.gnome.desktop.interface clock-show-weekday true
+gsettings set org.gnome.desktop.peripherals.touchpad disable-while-typing false
+dconf load / < resources/gnome-nav.ini
 
 cd ..
 rm -rf customization
