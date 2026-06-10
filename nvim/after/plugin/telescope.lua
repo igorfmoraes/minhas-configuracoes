932d28a036aa98d539da7661d44eb55c6075dfc8
Igor Moraes <igorfmoraes@proton.me>
2026-06-09 21:08:42 -0300

    Fuzyfinder com telescope

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,5 +1,4 @@
 local builtin = require("telescope.builtin")
 
 vim.keymap.set("n", "<Space>pf", builtin.find_files, {})
-vim.keymap.set("n", "<Space>pb", builtin.buffers, {})
-vim.keymap.set("n", "<Space>pp", builtin.git_files, {})
+vim.keymap.set("n", "<Space>ff", builtin.live_grep, {})
